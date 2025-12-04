7cf4307f5b82226f523bf2b6d7ec05926e2303db
BartekS5 <164517726+BartekS5@users.noreply.github.com>
2025-12-04 12:20:27 +0100

    day 2 part 2

--- day2/main.java
+++ day2/main.java
@@ -13,15 +13,25 @@ public class main {
                               .mapToLong(Long::parseLong)
                               .toArray();
             long invalidSum = 0;
+            /*
             for(int j = 0; j < numbers.length; j += 2) {
                 System.out.println("Checking range: " + numbers[j] + " to " + numbers[j + 1]);
                 for(long i = numbers[j]; i <= numbers[j + 1]; i++) {
-                    boolean hasDouble = false;
                     String numStr = Long.toString(i);
                     int lenHalf = numStr.length() / 2;
                     long devisor = (long) Math.pow(10, lenHalf);
 
                     if((i % devisor) == (i / devisor)){
+                        invalidSum += i;
+                    }
+                }
+            }
+             */
+            for(int j = 0; j < numbers.length; j += 2) {
+                System.out.println("Checking range: " + numbers[j] + " to " + numbers[j + 1]);
+                for(long i = numbers[j]; i <= numbers[j + 1]; i++) {
+                    String numStr = Long.toString(i);
+                    if (numStr.matches("(.+)\\1+")) {
                         invalidSum += i;
                     }
                 }
